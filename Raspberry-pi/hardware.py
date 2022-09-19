946e3af78c0ac116a52427dd224b088c294932c7
Ye0ngJae <kimjh38011@gmail.com>
2022-09-19 12:27:54 +0900

    add network info
    add network info module

--- Raspberry-pi/hardware.py
+++ Raspberry-pi/hardware.py
@@ -4,7 +4,7 @@ def cpu_info():
     cpu_info = os.popen("grep ^'model name' /proc/cpuinfo").read()
     cpu_info = cpu_info.replace('model name', '').split(':')
     
-    return cpu_info[1]
+    return cpu_info[1].replace('\n','')
 
 def ram_info():
     MemTotal = os.popen("grep ^'MemTotal' /proc/meminfo").read()
@@ -19,19 +19,24 @@ def ram_info():
     return "{}GB / {}GB".format(round(MemTotal,1), round(round(MemTotal,4) - round(MemFree,4), 2))
     
 def os_info():
-    os_info = ""
     os_info = os.popen("grep ^'PRETTY_NAME' /etc/os-release").read()
     
-    return os_info.replace("PRETTY_NAME=","").replace("'","")
+    return os_info.replace("PRETTY_NAME=","").replace('"',"").replace('\n','')
     
 def kernel_info():
-    kernel_info = os.popen("uname -r").read()
+    kernel_info = os.popen("uname -r").read().replace('\n','')
     
     return kernel_info
 
 def uptime_info():
-    uptime_info = os.popen("uptime").read()
+    uptime_info = os.popen("uptime").read().replace('\n','')
     uptime_info = uptime_info.split(",")
     
-    return uptime_info[0]
+    return uptime_info[0].replace("None","")
+
+def network_info():
+    network_info = os.popen("ip addr").read()
+    network_info = network_info.split("inet")
+    
+    return network_info[3].split("/")[0].replace(" ","")
 
