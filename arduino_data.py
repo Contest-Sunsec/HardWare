0ff88db017b7629dabd45eda616e2da58779fd6c
Ye0ngJae <kimjh38011@gmail.com>
2022-10-03 13:53:43 +0900

    Update arduino_data.py

--- arduino_data.py
+++ arduino_data.py
@@ -1,11 +1,12 @@
 from pyfirmata import *
+import time
 
 board = Arduino('/dev/ttyACM0') #시리얼 포트 지정
 seri = serial.Serail(board, baudrate=9600,timeout=None) #시리얼 포트 설정
 print("Serial name" + seri.name)#시리얼 포트 이름 출력
 
-seri.write('get_sensor'.encode()) #시리얼 포트에 데이터 전송
-
-content = seri.readline() #시리얼 포트에서 데이터 수신
-
-
+def get_sensor():
+    seri.write('get_sensor'.encode()) #시리얼 포트에 데이터 전송
+    time.sleep(1) #1초 대기
+    content = seri.readline() #시리얼 포트에서 데이터 수신
+    return content
