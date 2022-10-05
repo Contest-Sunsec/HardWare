8d74af8a255b62741abcbd431f0451591b3a34f8
Ye0ngJae <kimjh38011@gmail.com>
2022-10-05 09:58:24 +0900

    Create main.ino

---
+++ Arduino/main/main.ino
@@ -0,0 +1,24 @@
+
+int red = 6;
+int blue = 7;
+int green = 8;
+int soil_
+
+
+
+void setup() {
+
+  Serial.begin(9600);
+  digitalWrite(red,OUTPUT);
+  digitalWrite(blue,OUTPUT);
+  digitalWrite(green,OUTPUT);
+  
+  Serial.println("shangus");
+  
+
+}
+
+void loop() {
+
+
+}
